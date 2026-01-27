156e7734e96eb01cd67600f133ed2d0514f2b032
mohamdeesayed <mohamdeesayed@outlook.com>
2026-01-27 16:05:24 +0200

    add system app using translation

--- resources/views/partials/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="bg-gray-800 text-white py-8 text-center">
-    <p>&copy; {{ date('Y') }} خدمة الطباعة 3D</p>
-</footer>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,3 @@
 <?php
 
-use App\Http\Controllers\CalculatorController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
-
-
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
-
-Route::get('/calculator', [CalculatorController::class, 'index']);
-Route::post('/calculator', [CalculatorController::class, 'calculate']);
-
-Route::resource('orders', OrderController::class);
